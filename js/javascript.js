d33a9aa1bb54d9e8df690eb71bf2421d4cbedb99
Flatcova <m.covar20@gmail.com>
2016-04-20 21:30:11 -0500

    fix canvas

--- js/javascript.js
+++ js/javascript.js
@@ -5,3 +5,8 @@ $(document).ready(function(){
       $("#sub-con").slideToggle("slow");
     });
 });
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+var img = document.getElementById("scream");
+ctx.drawImage(img,10,10);
