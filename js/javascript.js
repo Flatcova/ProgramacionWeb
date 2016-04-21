610d862b9a42a595156a4a06176f2b812ffc5056
Flatcova <m.covar20@gmail.com>
2016-04-20 21:42:07 -0500

    done canvas

--- js/javascript.js
+++ js/javascript.js
@@ -5,8 +5,7 @@ $(document).ready(function(){
       $("#sub-con").slideToggle("slow");
     });
 });
-
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 var img = document.getElementById("scream");
 ctx.drawImage(img,10,10);
